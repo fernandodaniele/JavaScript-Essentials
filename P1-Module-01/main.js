930900e2fb7c0407244c40f23e103e357097cb1e
Fernando Daniele <fernandodaniele1993@gmail.com>
2025-05-08 16:01:32 +0200

    Add exercises from Module 1

---
+++ P1-Module-01/main.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+console.log("Fernando","Daniele","(",1993,")");
+console.log("My name concatenated:"+"Fernando "+"Daniele "+"(1993)");
+console.log("http://test.org");
